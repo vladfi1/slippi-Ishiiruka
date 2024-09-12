project-slippi/Ishiiruka

Add support for bot input over EXI interface  - Used for fast-forwarded inputs  - Helpful when training AI  - Only affects named-pipe input type

Source/Core/Core/HW/EXI_DeviceSlippi.h
@@ -83,6 +83,7 @@ class CEXISlippi : public IEXIDevice
 		CMD_GCT_LENGTH = 0xD3,
 		CMD_GCT_LOAD = 0xD4,
 		CMD_GET_DELAY = 0xD5,
+		CMD_OVERWRITE_INPUTS = 0xD6,
 		CMD_PREMADE_TEXT_LENGTH = 0xE1,
 		CMD_PREMADE_TEXT_LOAD = 0xE2,
 	};
@@ -135,6 +136,7 @@ class CEXISlippi : public IEXIDevice
 	    {CMD_GCT_LENGTH, 0x0},
 	    {CMD_GCT_LOAD, 0x4},
 	    {CMD_GET_DELAY, 0x0},
+	    {CMD_OVERWRITE_INPUTS, 0x0},
 	    {CMD_PREMADE_TEXT_LENGTH, 0x2},
 	    {CMD_PREMADE_TEXT_LOAD, 0x2},
 	};
@@ -212,6 +214,7 @@ class CEXISlippi : public IEXIDevice
 	void prepareGctLength();
 	void prepareGctLoad(u8 *payload);
 	void prepareDelayResponse();
+	void prepareOverwriteInputs();
 	void preparePremadeTextLength(u8 *payload);
 	void preparePremadeTextLoad(u8 *payload);
 

vladfi1/slippi-Ishiiruka

Source/Core/Core/HW/EXI_DeviceSlippi.h
@@ -95,6 +95,7 @@ class CEXISlippi : public IEXIDevice
 		CMD_PLAY_MUSIC = 0xD6,
 		CMD_STOP_MUSIC = 0xD7,
 		CMD_CHANGE_MUSIC_VOLUME = 0xD8,
+		CMD_OVERWRITE_INPUTS = 0xD9,
 		CMD_PREMADE_TEXT_LENGTH = 0xE1,
 		CMD_PREMADE_TEXT_LOAD = 0xE2,
 	};
@@ -154,6 +155,7 @@ class CEXISlippi : public IEXIDevice
 	    {CMD_PLAY_MUSIC, static_cast<u32>(sizeof(SlippiExiTypes::PlayMusicQuery) - 1)},
 	    {CMD_STOP_MUSIC, 0x0},
 	    {CMD_CHANGE_MUSIC_VOLUME, static_cast<u32>(sizeof(SlippiExiTypes::ChangeMusicVolumeQuery) - 1)},
+	    {CMD_OVERWRITE_INPUTS, 0x0},
 	    {CMD_PREMADE_TEXT_LENGTH, 0x2},
 	    {CMD_PREMADE_TEXT_LOAD, 0x2},
 	};
@@ -239,6 +241,7 @@ class CEXISlippi : public IEXIDevice
 	void prepareGctLength();
 	void prepareGctLoad(u8 *payload);
 	void prepareDelayResponse();
+	void prepareOverwriteInputs();
 	void preparePremadeTextLength(u8 *payload);
 	void preparePremadeTextLoad(u8 *payload);
 